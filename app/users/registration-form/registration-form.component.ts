2188334cd08b22a3ab402d98902915d97dbb4588
Invis1ble <invisiblexman2010@gmail.com>
2016-08-28 13:50:26 +0300

    Improved validation of the registration form

--- app/users/registration-form/registration-form.component.ts
+++ app/users/registration-form/registration-form.component.ts
@@ -49,17 +49,46 @@ export class RegistrationFormComponent {
         return this.form.controls[controlName].getError(errorCode);
     }
 
+    private getControlErrors(controlName: string): Array<Object> | null {
+        return this.getFieldErrors(this.errors, controlName);
+    }
+
+    private getFieldErrors(errors: FormErrors, path: string): Array<Object> | null {
+        if (undefined === errors.children) {
+            return null;
+        }
+
+        let segmentEnd = path.indexOf('[');
+
+        if (segmentEnd !== -1) {
+            let segment = path.substr(0, segmentEnd);
+
+            if (undefined === errors.children[segment]) {
+                return null;
+            }
+
+            return this.getFieldErrors(errors.children[segment], path.replace(/^\w+\[(\w*)]/, '$1'));
+        }
+
+        if (undefined === errors.children[path] || undefined === errors.children[path].errors) {
+            return null;
+        }
+
+        return errors.children[path].errors;
+    }
+
     private hasControlError(controlName: string, errorCode: string): boolean {
         return this.form.controls[controlName].hasError(errorCode);
     }
 
     private isControlValid(controlName: string): boolean {
-        return !(
-            this.errors.children &&
-            this.errors.children[controlName] &&
-            this.errors.children[controlName].errors &&
-            this.errors.children[controlName].errors.length
-        ) && (this.form.controls[controlName].valid || this.form.controls[controlName].pristine);
+        if (this.form.controls[controlName].valid || this.form.controls[controlName].pristine) {
+            let errors = this.getControlErrors(controlName);
+
+            return errors === null || errors.length === 0;
+        }
+
+        return false;
     }
 
     private onSubmit() {
