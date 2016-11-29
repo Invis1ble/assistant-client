46186776dde1d8ab9f54940b3adf363f4652efda
Invis1ble <invisiblexman2010@gmail.com>
2016-11-29 17:29:10 +0300

    Deduplicated tokenGetter implementation, closes #32

--- src/app/security/jwt/jwt.module.ts
+++ src/app/security/jwt/jwt.module.ts
@@ -1,10 +1,13 @@
+import { Http } from '@angular/http';
 import { NgModule } from '@angular/core';
 
-import { JwtHelper, provideAuth } from 'angular2-jwt';
+import { AuthConfig, AuthHttp, JwtHelper } from 'angular2-jwt';
 
-import { CONFIG_DEV } from '../../config/config.dev';
+import { CONFIG } from '../../config/config-token';
+import { Config } from '../../config/config';
 import { JwtResponseBodyToJwtTransformer } from './jwt-response-body-to-jwt.transformer';
 import { JwtService } from './jwt.service';
+import { JwtStorage } from './jwt-storage';
 import { JwtToRefreshTokenRequestBodyTransformer } from './jwt-to-refresh-token-request-body.transformer';
 import { RestModule } from '../../rest/rest.module';
 
@@ -17,20 +20,19 @@ import { RestModule } from '../../rest/rest.module';
         JwtResponseBodyToJwtTransformer,
         JwtService,
         JwtToRefreshTokenRequestBodyTransformer,
-        provideAuth({
-            globalHeaders: [{'Content-Type': 'application/json'}],
-            // TODO: use JwtLocalStorage.prototype.getToken as tokenGetter
-            tokenGetter: () => {
-                let jwt = localStorage.getItem(CONFIG_DEV.jwtName);
-
-                if (null === jwt) {
-                    return null;
-                }
-
-                return JSON.parse(jwt).token;
+        {
+            provide: AuthHttp,
+            useFactory: (http: Http, config: Config, jwtStorage: JwtStorage) => {
+                return new AuthHttp(new AuthConfig({
+                    globalHeaders: [{'Content-Type': 'application/json'}],
+                    tokenGetter() {
+                        return jwtStorage.getToken().token;
+                    },
+                    tokenName: config.jwtName
+                }), http);
             },
-            tokenName: CONFIG_DEV.jwtName
-        })
+            deps: [Http, CONFIG, JwtStorage]
+        }
     ]
 })
 export class JwtModule {
